adc696b7826622b128ceeb02b0207cebbade39f9
Glitch (head-pars) <none>
2018-01-10 15:38:24 +0000

    🌾👜 Checkpoint server.js:415617/225 package.json:415617/2

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 
 // init project
 var express = require('express');
+var ua = require('ua-parser-js');
+
 var app = express();
 
 // we've started you off with Express, 
@@ -13,25 +15,20 @@ app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  //response.sendFile(__dirname + '/views/index.html');
+  var soft = ua(request.get('user-agent'));
+  var ip = request.headers['x-forwarded-for'] ||
+     request.connection.remoteAddress ||
+     request.socket.remoteAddress ||
+     request.connection.socket.remoteAddress;
+  var lang = request.get('accept-language');
+  var userObject = {
+  "ip-address": ip,
+  "language": lang.split(',')[0],
+  "OS": soft.os.name + " " + soft.os.version
+  }
+  response.send(userObject);
 });
-
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
